b17a14a2f27dac8044c79cdc3aa30eae6533392c
outershpongolia <milenavuckovic3@gmail.com>
2024-08-08 14:26:54 +0200

    style tracker timestamp

--- src/components/Tracker/TrackerStamp/TrackerStamp.tsx
+++ src/components/Tracker/TrackerStamp/TrackerStamp.tsx
@@ -1,5 +1,6 @@
 import React, { useCallback } from "react"
 import './TrackerStamp.scss'
+import { GiTargetPrize } from "react-icons/gi";
 
 interface ITrackerStampProps {
   name: string
@@ -18,16 +19,17 @@ export const TrackerStamp: React.FC<ITrackerStampProps> = ({ name, startDate, en
       className="tracker-stamp"
       onClick={handleNavigateToTracker}
     >
-      <div className="tracker-stamp__name">{name}</div>
+      <div className="tracker-stamp__info">
+        <div className="tracker-stamp__name">{name}</div>
+        {description && <div className="tracker-stamp__description">{description}</div>}
+      </div>
 
-      {description && <div className="tracker-stamp__description">{description}</div>}
+      <div className="tracker-stamp__score">
+        <GiTargetPrize className="tracker-stamp__icon" />
 
-      <div className="tracker-stamp__dates">
-        <div className="tracker-stamp__date">
-          <span>start:</span> {startDate?.toString().slice(0, 10)}
-        </div>
-        <div className="tracker-stamp__date">
-          <span>end:</span> {endDate?.toString().slice(0, 10)}
+        <div className="tracker-stamp__data">
+          <div className="tracker-stamp__number">111</div>
+          <div className="tracker-stamp__text">days left</div>
         </div>
       </div>
     </div>
